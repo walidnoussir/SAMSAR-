3dfa135318578100a32d48d1295e79687bac7753
walidnoussir <noussirw@gmail.com>
2026-08-03 11:33:52 +0100

    connect the app with mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,22 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to start server");
+    console.error(error);
+  }
+};
+
+startServer();
