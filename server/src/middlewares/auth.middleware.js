e7fad9f37c7510b9be03c2be63335a7beecd1992
walidnoussir <noussirw@gmail.com>
2026-08-04 12:37:45 +0100

    middleware for validation is done

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,19 @@
+import { validationResult } from "express-validator";
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  return res.status(400).json({
+    success: false,
+    errors: errors.array().map((error) => ({
+      field: error.path,
+      message: error.msg,
+    })),
+  });
+};
+
+export default validate;
