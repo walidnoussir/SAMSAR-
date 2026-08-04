a5547909987e57ec93eaa61c670eee82f2be6e85
walidnoussir <noussirw@gmail.com>
2026-08-04 12:53:42 +0100

    auth controller is completed with service functionalities

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import * as authService from "../services/auth.service.js";
+
+export const register = async (req, res, next) => {
+  try {
+    const result = await authService.registerUser(req.body);
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const result = await authService.loginUser(
+      req.body.email,
+      req.body.password,
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      ...result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
