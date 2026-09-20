3f2f3935562d5a88af241211d5d0902d0fedafcd
walidnoussir <noussirw@gmail.com>
2026-09-20 13:06:29 +0000

    ui updates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <App />
         <Toaster
-          position="top-right"
+          position="bottom-right"
           toastOptions={{
             duration: 4000,
             style: {
